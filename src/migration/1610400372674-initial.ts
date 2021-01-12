05346e4a96ba8b607c2a90b74d5e51b40dabcd15
Leonardo Cartaxo <leo.cartaxo@gmail.com>
2021-01-11 22:12:53 -0300

    formated initial sql

--- src/migration/1610400372674-initial.ts
+++ src/migration/1610400372674-initial.ts
@@ -1,14 +1,24 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class initial1610400372674 implements MigrationInterface {
-    name = 'initial1610400372674'
+  name = 'initial1610400372674';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "createdOn" TIMESTAMP NOT NULL DEFAULT now(), "updatedOn" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "name" character varying NOT NULL, "email" character varying NOT NULL, "hashedPassword" character varying NOT NULL, "roles" character varying array NOT NULL DEFAULT '{USER}'::varchar[], CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE "users" (
+            "id" SERIAL NOT NULL,
+            "createdOn" TIMESTAMP NOT NULL DEFAULT now(),
+            "updatedOn" TIMESTAMP NOT NULL DEFAULT now(),
+            "deletedAt" TIMESTAMP,
+            "name" character varying NOT NULL,
+            "email" character varying NOT NULL,
+            "hashedPassword" character varying NOT NULL,
+            "roles" character varying array NOT NULL DEFAULT '{USER}' :: varchar [],
+            CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"),
+            CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+        )`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "users"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
 }
